2acc1624cb1def463f86c36cc955c40917a4e5f8
aczender <czender.a@gmail.com>
2018-10-29 14:18:46 +0100

    Commit until 11 - VariableMutation

---
+++ week-01/day-04/intelliJ projects/src/Bmi.java
@@ -0,0 +1,10 @@
+public class Bmi {
+    public static void main(String[] args) {
+        double massInkg = 81.2;
+        double heightInM = 1.78;
+
+        System.out.println(massInkg / (Math.pow(heightInM, 2)));
+
+
+    }
+}
---
+++ week-01/day-04/intelliJ projects/src/DefineBasicInfo.java
@@ -0,0 +1,10 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Andras";
+        int age = 31;
+        double height = 1.87;
+        boolean marriage = false;
+
+        System.out.println(name + "\r\n" + age + "\r\n" + height + "\r\n" + marriage);
+    }
+}
