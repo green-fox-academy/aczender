baa92748490cdb6cdce7f960530e61181c1dca55
aczender <czender.a@gmail.com>
2018-10-30 11:49:31 +0100

    Until 'Greeter function'

---
+++ week-01/day-05/src/Greet.java
@@ -0,0 +1,13 @@
+public class Greet {
+
+    public static void main(String[] args) {
+        String a1 = "Béla";
+        greet(a1);
+
+    }
+
+    public static void greet(String a1) {
+
+        System.out.println("Greating dear, " + a1);
+    }
+}
