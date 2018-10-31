babf8aa40df63cc8b6f7b4f73acf6ac6d2b0b331
aczender <czender.a@gmail.com>
2018-10-31 15:05:38 +0100

    Matrix

--- week-01/day-05/src/Matrix.java
+++ week-01/day-05/src/Matrix.java
@@ -2,14 +2,19 @@ import java.util.Arrays;
 
 public class Matrix {
     public static void main(String[] args) {
-        int[][] matrix = {{1}, {0}};
-        for (int i = 1; i < 2; i++) {
-            //if (matrix[0][0] )
-            System.out.print((matrix[0][0]));
+        int[][] matrix = new int[4][4];
 
-            /*for (int j = 0; j < 3; j++) {
-                System.out.print((matrix[1][0]));
-            }*/
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j) {
+                    System.out.print(1);
+                } else {
+                    System.out.print(matrix[i][j]);
+                }
+
+            }
+            System.out.println();
+
         }
     }
 }
