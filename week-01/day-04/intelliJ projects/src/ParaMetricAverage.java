d822d9efdd204f63345ee737a8f27a97d82c59d8
aczender <czender.a@gmail.com>
2018-11-01 19:45:59 +0100

    ParaMetricAverage

---
+++ week-01/day-04/intelliJ projects/src/ParaMetricAverage.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ParaMetricAverage {
+    public static void main(String[] args) {
+
+        System.out.print("Please provide a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        int input = 0;
+        int sum = 0;
+        System.out.println("Please add more numbers: ");
+        for (int i = 0; i < number; i++) {
+            sum += scanner.nextInt();
+        }
+
+
+        double average = sum / number;
+
+
+        System.out.println("Sum: " + sum + "\r\n" + "Average: " + average);
+    }
+}
