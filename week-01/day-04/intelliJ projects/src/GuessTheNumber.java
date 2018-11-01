d8f05d82e5890eac27d4ffcaaf41259cc078100d
aczender <czender.a@gmail.com>
2018-11-01 18:28:48 +0100

    GuessTheNumber

--- week-01/day-04/intelliJ projects/src/GuessTheNumber.java
+++ week-01/day-04/intelliJ projects/src/GuessTheNumber.java
@@ -1,27 +1,41 @@
-/*import java.util.Random;
+import java.util.Random;
 import java.util.Scanner;
+// Write a program that stores a number, and the user has to figure it out.
+// The user can input guesses, after each guess the program would tell one
+// of the following:
+//
+// The stored number is higher
+// The stored number is lower
+// You found the number: 8
 
 public class GuessTheNumber {
     public static void main(String[] args) {
 
         Random random = new Random();
-        int stored = random.nextInt(50);
+        int stored = random.nextInt(20);
         int number;
+        int guess = 0;
 
 
         do {
             System.out.print("Guess a number: ");
             Scanner scanner = new Scanner(System.in);
-            int number = scanner.nextInt();
+            number = scanner.nextInt();
+            guess++;
+
 
             if (stored > number) {
                 System.out.println("The stored number is higher");
             } else if (stored < number) {
                 System.out.println("The stored number is lower");
-            } else {
-                System.out.println("You found the number: " + stored);
             }
         }
-        while(number != stored);
+        while (stored != number && guess < 10);
+        if (stored == number) {
+            System.out.println("You won");
+        } else if (guess == 10) {
+            System.out.println("You lost");
+        }
+
     }
-}*/
+}
