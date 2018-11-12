9ebc12a5757b968f73a9f362bc591839fca00e53
aczender <czender.a@gmail.com>
2018-11-12 20:02:48 +0100

    Doubled

---
+++ week-04/day-01/src/Doubled.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Doubled {
+
+    public static void main(String[] args) {
+        // Create a method that decrypts the duplicated-chars.txt
+        toDecode();
+
+    }
+
+    public static void toDecode() {
+        Path filePath = Paths.get("duplicated-chars.txt");
+        try {
+            List<String> lines = Files.readAllLines(filePath);
+            for (String line : lines) {
+                for (int i = 0; i < line.length(); i++) {
+                    if (i % 2 == 0) {
+                        System.out.print(line.charAt(i));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
