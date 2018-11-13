dd8b21415df0baf80c1385ed37ee1aa252ba8b84
aczender <czender.a@gmail.com>
2018-11-13 11:17:13 +0100

    Post-it

---
+++ week-04/day-02/src/postit/PostIt.java
@@ -0,0 +1,16 @@
+package postit;
+
+public class PostIt {
+    String backgroundColor;     //field variables
+    String about;
+    String textColor;
+
+    public PostIt(String backgroundColor, String about, String textColor) {
+        this.backgroundColor = backgroundColor;
+        this.about = about;
+        this.textColor = textColor;
+    }
+
+}
+
+
---
+++ week-04/day-02/src/postit/Postits.java
@@ -0,0 +1,12 @@
+package postit;
+
+public class Postits {
+
+    public static void main(String[] args) {
+        PostIt first = new PostIt("red","about","blue");
+        PostIt second = new PostIt("blue", "this", "yellow");
+        PostIt third = new PostIt("pink", "another", "blue");
+    }
+
+
+}
