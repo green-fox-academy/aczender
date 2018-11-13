72227db69df0d18256ff74351e6fca673dbf5f29
aczender <czender.a@gmail.com>
2018-11-13 22:17:25 +0100

    TeachersandStudents

---
+++ week-04/day-02/src/teachersandstudents/Student.java
@@ -0,0 +1,14 @@
+package teachersandstudents;
+
+public class Student {
+
+    public static void learn(){
+        String learn = "math";
+    }
+
+    public void question(Teacher teacher){
+        Teacher.answer();
+    }
+
+
+}
---
+++ week-04/day-02/src/teachersandstudents/Teacher.java
@@ -0,0 +1,12 @@
+package teachersandstudents;
+
+public class Teacher {
+    public static void answer(){
+        String answer = "this";
+    }
+
+    public static void teach(Student student) {
+        Student.learn();
+    }
+
+}
