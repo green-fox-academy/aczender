78c79e75ae6dc658b81f747aefa1b0d98f10730f
aczender <czender.a@gmail.com>
2018-11-13 17:47:48 +0100

    FleetOfThings

---
+++ week-04/day-02/src/fleetofthings/FleetOfThings.java
@@ -0,0 +1,30 @@
+package fleetofthings;
+
+public class FleetOfThings {
+
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+
+        Thing milk = new Thing ("Get milk");
+        Thing obstacles = new Thing ("Remove the obstacles");
+        Thing standUp = new Thing ("Stand up");
+        Thing eatLunch = new Thing("Eat lunch");
+
+        standUp.complete();
+        eatLunch.complete();
+
+        fleet.add(milk);
+        fleet.add(obstacles);
+        fleet.add(standUp);
+        fleet.add(eatLunch);
+
+
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+
+        System.out.println(fleet);
+    }
+}
