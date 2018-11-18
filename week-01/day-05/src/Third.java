c7e7b0e257857b5dd165e0012ef70702a06bd051
aczender <czender.a@gmail.com>
2018-11-18 12:01:47 +0100

    Third

--- week-01/day-05/src/Third.java
+++ week-01/day-05/src/Third.java
@@ -1,6 +1,10 @@
 public class Third {
     public static void main(String[] args) {
         int[] q = {4, 5,6,7};
-        System.out.print(q[2]);
+        System.out.println(q[2]);
+
+        for (int i = 0; i < q.length ; i++) {
+        }
+        System.out.println(q[2]);
     }
 }
