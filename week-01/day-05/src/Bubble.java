0f48f62b6f5d792d6dff2f198b399e6cf5e9e62f
aczender <czender.a@gmail.com>
2018-11-18 14:25:50 +0100

    BubbleSort

---
+++ week-01/day-05/src/Bubble.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Bubble {
+    public static void main(String[] args) {
+        System.out.println(bubble(new int[]{9, 12, 33, 1, 9, 5}));
+    }
+
+    public static String bubble(int[] ar) {
+        int temp = 0;
+
+        for (int i = ar.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+
+                if (ar[j] > ar[j + 1]) {
+                    temp = ar[j];
+                    ar[j] = ar[j + 1];
+                    ar[j + 1] = temp;
+                }
+            }
+        }
+        String bubles  = Arrays.toString(ar);
+        return bubles;
+
+    }
+
+}
