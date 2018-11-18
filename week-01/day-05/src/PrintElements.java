45c78c4ab23ecb28cfa0cd020ab1e3319c634164
aczender <czender.a@gmail.com>
2018-11-18 12:08:43 +0100

    PrintElements

--- week-01/day-05/src/PrintElements.java
+++ week-01/day-05/src/PrintElements.java
@@ -4,5 +4,9 @@ public class PrintElements {
     public static void main(String[] args) {
         int[] numbers = {4, 5, 6, 7};
         System.out.println(Arrays.toString(numbers));
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+        }
     }
 }
