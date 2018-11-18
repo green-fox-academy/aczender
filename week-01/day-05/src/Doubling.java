a07bf35ae2c5bf5338f5fb16260047b27d77d094
aczender <czender.a@gmail.com>
2018-11-18 12:02:42 +0100

    Doubling

--- week-01/day-05/src/Doubling.java
+++ week-01/day-05/src/Doubling.java
@@ -1,3 +1,6 @@
+// - Create an integer variable named `baseNum` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Print the result of `doubling(baseNum)`
 public class Doubling {
 
     public static void main(String[] args) {
