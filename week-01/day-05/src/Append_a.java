5a841d75eed6c1664ac082abacb9e3d55b722d86
aczender <czender.a@gmail.com>
2018-11-18 12:19:20 +0100

    Append_a

--- week-01/day-05/src/Append_a.java
+++ week-01/day-05/src/Append_a.java
@@ -7,5 +7,10 @@ public class Append_a {
      for (String a : animals) {
             System.out.print(a + "a ");
         }
+
+        for (int i = 0; i < animals.length ; i++) {
+            System.out.println(animals[i] + "a");
+        }
     }
+
 }
