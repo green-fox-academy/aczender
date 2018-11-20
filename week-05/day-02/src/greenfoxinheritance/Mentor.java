067d7666a3c9f36c4f3fa927ae86436e5f49f0aa
aczender <czender.a@gmail.com>
2018-11-20 11:37:23 +0100

    Mentor

---
+++ week-05/day-02/src/greenfoxinheritance/Mentor.java
@@ -0,0 +1,24 @@
+package greenfoxinheritance;
+
+public class Mentor extends Person {
+    String level;
+
+    Mentor(String name, int age, String gender, String level) {
+        super(name, age, gender);
+        this.level = level;
+    }
+
+    Mentor() {
+        super();
+        this.level = "intermediate";
+    }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + name + ", a " + age + " years old " + gender + " " + level + " mentor.");
+    }
+
+    public void getGoal() {
+        System.out.println("Educate brilliant junior software developers.");
+    }
+
+}
