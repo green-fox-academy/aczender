419125727735804737d03c8c426a911a3c84aa59
aczender <czender.a@gmail.com>
2018-11-22 13:53:04 +0100

    Anagram fix

--- week-05/day-04/src/Anagram.java
+++ week-05/day-04/src/Anagram.java
@@ -4,17 +4,17 @@ public class Anagram {
 
     public static boolean anagram(String first, String second) {
 
-        if (first == null || second == null)
-            return false;
-        if (first.length() != second.length())
-            return false;
+        //NOT REQUIRED LINES
+//        if (first == null || second == null)
+//            return false;
+//        if (first.length() != second.length())
+//            return false;
         char[] case1 = (first.toLowerCase()).toCharArray();
         char[] case2 = (second.toLowerCase()).toCharArray();
         Arrays.sort(case1);
         Arrays.sort(case2);
         if (Arrays.equals(case1, case2))
             return true;
-
         else
             return false;
     }
