c40087548aab01da8a00cb9d1426f4e1a43178fe
aczender <czender.a@gmail.com>
2018-11-22 22:11:50 +0100

    DoubleItems

--- week-01/day-05/src/DoubleItems.java
+++ week-01/day-05/src/DoubleItems.java
@@ -1,17 +1,12 @@
 public class DoubleItems {
     public static void main(String[] args) {
         int[] numList = {3, 4, 5, 6, 7};
-        foreach(numList);
+
 
         for (int i = 0; i < numList.length; i++) {
-            System.out.println(numList[i] * 2);
+            numList[i] = numList[i] * 2;
+            System.out.println(numList[i]);
 
-        }
-    }
-
-    public static void foreach(int[] amount) {
-        for (int a : amount) {
-            System.out.print(a*2 + " ");
         }
     }
 }
