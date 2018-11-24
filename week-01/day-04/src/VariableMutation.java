38f1a644192d42acaa2702dcc83dff615dc14482
aczender <czender.a@gmail.com>
2018-11-24 14:36:20 +0100

    VariableMutation

--- week-01/day-04/src/VariableMutation.java
+++ week-01/day-04/src/VariableMutation.java
@@ -39,7 +39,11 @@ public class VariableMutation {
         int g1 = 350;
         int g2 = 200;
         // tell if the double of g2 is bigger than g1 (print as a boolean)
-        System.out.println(g2 > g1);
+        if ((g2 * 2) > g1) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
 
         int h = 135798745;
         // tell if it has 11 as a divisor (print as a boolean)
@@ -49,11 +53,15 @@ public class VariableMutation {
         int i1 = 10;
         int i2 = 3;
         // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
-        System.out.println(i1 > (Math.pow(i2,2)) && i1 < (Math.pow(i2,3)));
+        System.out.println(i1 > (Math.pow(i2, 2)) && i1 < (Math.pow(i2, 3)));
 
         int j = 1521;
         // tell if j is dividable by 3 or 5 (print as a boolean)
-        System.out.println(j % 3 == 0 || j % 5 == 0);
+        if (j % 3 == 0 || j % 5 == 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
 
         String k = "Apple";
         //fill the k variable with its content 4 times
