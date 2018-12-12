0955cdbb82da7da7bdd2e9feae350d181b2f7a46
aczender <czender.a@gmail.com>
2018-12-12 19:25:08 +0100

    BookControl

---
+++ week-08/bookcontrol/src/main/java/com/greenfoxacademy/bookcontrol/BookcontrolApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.bookcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcontrolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookcontrolApplication.class, args);
+    }
+}
