348f214cf12fbbfc7b077e08f5b238db78fc941a
aczender <czender.a@gmail.com>
2018-12-13 14:37:12 +0100

    Colors

---
+++ week-08/hellobean/src/main/java/com/greenfoxacademy/hellobean/model/GreenColor.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.hellobean.model;
+
+import com.greenfoxacademy.hellobean.MyColor;
+import com.greenfoxacademy.hellobean.service.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GreenColor implements MyColor {
+    @Autowired
+    Printer printer;
+
+    @Override
+    public void printColor() {
+        printer.log("this is green");
+    }
+}
