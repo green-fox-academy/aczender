28429d9938fc0690906df0800544004d4d043d1a
aczender <czender.a@gmail.com>
2018-12-16 07:58:44 +0100

    GreenFoxClass - add student

--- week-08/greenfoxclass/src/main/java/com/greenfoxacademy/greenfoxclass/HomePageController.java
+++ week-08/greenfoxclass/src/main/java/com/greenfoxacademy/greenfoxclass/HomePageController.java
@@ -4,6 +4,8 @@ import com.greenfoxacademy.greenfoxclass.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +14,7 @@ public class HomePageController {
     StudentService studentService;
 
     @Autowired
-    public HomePageController(StudentService studentService){
+    public HomePageController(StudentService studentService) {
         this.studentService = studentService;
     }
 
@@ -22,8 +24,19 @@ public class HomePageController {
     }
 
     @RequestMapping("/gfa/list")
-    public String studentList(Model model){
-        model.addAttribute("students",studentService.findAll());
+    public String studentList(Model model) {
+        model.addAttribute("students", studentService.findAll());
         return "studentlist";
+    }
+
+    @GetMapping("/gfa/add")
+    public String addStudentForm(Model model, @ModelAttribute(name = "student") String student) {
+        model.addAttribute("student", student);
+        return "addstudent";
+    }
+    @GetMapping(path = "/gfa/save")
+    public String addStudent(@ModelAttribute(name = "student") String student) {
+        studentService.save(student);
+        return "redirect:/gfa/list";
     }
 }
