e1ced154c52b220b2415dd429c79eb9acd820b20
aczender <czender.a@gmail.com>
2019-02-06 21:06:10 +0100

    Main class refreshed

--- week-05/day-03/src/charsequence/Main.java
+++ week-05/day-03/src/charsequence/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Gnirts s = new Gnirts("example");
 
-
         System.out.println(s.charAt(4));
-        System.out.println(s.subSequence(3,5));
+        System.out.println(s.subSequence(3, 5));
         System.out.println(s.length());
+
 
         Shifter p = new Shifter("otherexample", 2);
         System.out.println(p.charAt(1));
-        System.out.println(p.subSequence(3,5));
+        System.out.println(p.subSequence(3, 5));
         System.out.println(p.length());
     }
 }
