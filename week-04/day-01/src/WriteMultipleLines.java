3f3063ef3a099a284d2335ff3e387e3aec7c50b4
aczender <czender.a@gmail.com>
2019-03-03 15:05:43 +0100

    WriteSingLine

--- week-04/day-01/src/WriteMultipleLines.java
+++ week-04/day-01/src/WriteMultipleLines.java
@@ -8,18 +8,20 @@ import java.util.Scanner;
 
 public class WriteMultipleLines {
     public static void main(String[] args) {
-        multiple("multiple.txt", "apple", 6);
+        multiple();
     }
 
-    public static void multiple(String path, String input, int result) {
-
-
+    public static void multiple() {
+        Scanner scanner = new Scanner(System.in);   //opens the scanner
+        String line = scanner.next();               //asks for the 'line' content from user input
+        int number = scanner.nextInt();             //asks for the number of times to write
         List<String> content = new ArrayList<>();   //creates a String that will be added to the file
+        content.add(line);                          //this 'line' will be added to the file
         try {
-            for (int i = 0; i < result; i++) {
-                content.add(input);                       //this writes the content to the file
+            for (int i = 0; i < number; i++) {
+                content.add(line);                       //this writes the content to the file
             }
-            Path filePath = Paths.get(path);    //opens and/or creates the file
+            Path filePath = Paths.get("multiple.txt");    //opens and/or creates the file
             Files.write(filePath, content);
 
         } catch (IOException e) {
